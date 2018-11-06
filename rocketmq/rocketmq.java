98a0c24b1d3578339169c8f3afce342d8be5d639
huatuo <id512@126.com>
2018-11-06 09:20:43 +0800

    modify mongod.conf

---
+++ rocketmq/rocketmq.java
@@ -0,0 +1,14 @@
+有三种方式
+        可同步的、可靠的异步和单向传输
+
+
+<dependency>
+<groupId>org.apache.rocketmq</groupId>
+<artifactId>rocketmq-client</artifactId>
+<version>4.3.0</version>
+</dependency>
+
+
+
+        1.download
+        mvn -Prelease-all -DskipTests clean install -U
