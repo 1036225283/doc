3fb59575cd52c607829e2cd39744737a883b2830
1036225283 <id512@126.com>
2019-06-18 22:39:14 +0800

    修改mongo.conf

--- develop-tool/shell/string_split.sh
+++ develop-tool/shell/string_split.sh
@@ -6,11 +6,11 @@ array=(${string//,/ })
 echo "获取数组的值：${array_name[index]}"
 echo ${array[0]}
 a=${array[0]}
+
 a=redis
 echo $a
 ID=`ps -ef|grep $a|grep -v grep|awk  '{print  $2}' `
-echo $ID
-for var in ${ID[@]}
+for var in ${array[@]}
 do
    echo $var
 done
