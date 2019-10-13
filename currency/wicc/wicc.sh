661d237666459f63ac02806333b8cfcdd152095b
1036225283 <id512@126.com>
2019-10-13 18:19:09 +0800

    add coin wicc

--- currency/wicc/wicc.sh
+++ currency/wicc/wicc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-curl -u Waykichain:admin -d '{"jsonrpc":"2.0","id":"curltext","method":"listaddr","params":[]}' -H 'content-type:application/json;' http://127.0.0.1:8920
+# list address
+curl -u wiccuser:wicc1000 -d '{"jsonrpc":"2.0","id":"curltext","method":"listaddr","params":[]}' -H 'content-type:application/json;' http://127.0.0.1:6967
 
-curl -u wiccuser:wicc1000 -d '{"jsonrpc":"2.0","id":"curltext","method":"listaddr","params":[]}' -H 'content-type:application/json;' http://127.0.0.1:6968
+# create address
+curl -u Waykichain:admin -d '{"jsonrpc":"2.0","id":"curltext","method":"getnewaddr","params":[]}' -H 'content-type:application/json;' http://127.0.0.1:6967
