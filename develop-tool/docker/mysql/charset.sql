caea1b5fe3ced66ee0c4d8045780657e678b4d80
1036225283 <id512@126.com>
2020-09-05 00:29:14 +0800

    add--charset.sql

---
+++ develop-tool/docker/mysql/charset.sql
@@ -0,0 +1,20 @@
+
+
+
+show variables like 'character%';
+
+set names utf8;
+
+
+# 表字符编码
+SHOW CREATE TABLE tbStockModel;
+
+# 修改字符编码
+ALTER TABLE tbStockModel CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+
+show variables where Variable_name like '%char%';
+
+show create database stock.tbUserLog;
+ set character_set_server=utf8mb4;
+alter table tbUserLog character set utf8mb4
